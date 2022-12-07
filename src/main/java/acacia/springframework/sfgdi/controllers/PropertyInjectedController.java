01576dd4e0be932cb376851a2515c9b58bbb62af
acacia1021 <acacia1021@gmail.com>
2022-12-07 13:18:09 -0500

    Tests passing, but not showing to console Video 34

---
+++ src/main/java/acacia/springframework/sfgdi/controllers/PropertyInjectedController.java
@@ -0,0 +1,13 @@
+package acacia.springframework.sfgdi.controllers;
+
+import acacia.springframework.sfgdi.services.GreetingService;
+
+public class PropertyInjectedController {
+
+    public GreetingService greetingService;
+
+    public String getGreeting(){
+        return greetingService.sayGreeting();
+    }
+    
+}
