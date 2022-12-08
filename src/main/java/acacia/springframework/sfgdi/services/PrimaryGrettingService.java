5c36079717da4a06b46f05352b37cbd18d390013
acacia1021 <acacia1021@gmail.com>
2022-12-08 09:42:27 -0500

    Video 37

---
+++ src/main/java/acacia/springframework/sfgdi/services/PrimaryGrettingService.java
@@ -0,0 +1,15 @@
+package acacia.springframework.sfgdi.services;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+@Primary
+@Service
+public class PrimaryGrettingService implements GreetingService{
+
+    @Override
+    public String sayGreeting(){
+        return "Hello World - From the PRIMARY Bean";
+    }
+    
+}
