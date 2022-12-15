5ca5e8409040621606d4bdd7a3356a1c0ecc8f85
acacia1021 <acacia1021@gmail.com>
2022-12-15 17:05:42 -0500

    Video #75 Dependency Injection Configuration

--- src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 
+import guru.springframework.sfgdi.repositories.EnglishGreetingRepository;
+import guru.springframework.sfgdi.repositories.EnglishGreetingRepositoryImpl;
 import guru.springframework.sfgdi.services.ConstructorGreetingService;
 import guru.springframework.sfgdi.services.I18NSpanishService;
 import guru.springframework.sfgdi.services.I18nEnglishGreetingService;
@@ -15,21 +17,28 @@ import guru.springframework.sfgdi.services.SetterInjectedGreetingService;
 @Configuration
 public class GreetingServiceConfig {
 
-    @Profile({"ES", "default"})
+    @Profile({ "ES", "default" })
     @Bean("i18nService")
-    I18NSpanishService i18NSpanishService(){
+    I18NSpanishService i18NSpanishService() {
         return new I18NSpanishService();
+    }
+
+    @Bean
+    EnglishGreetingRepository englishGreetingRepository() {
+        return new EnglishGreetingRepositoryImpl() {
+
+        };
     }
 
     @Profile("EN")
     @Bean
-    I18nEnglishGreetingService i18nService(){
-        return new I18nEnglishGreetingService();
+    I18nEnglishGreetingService i18nService(EnglishGreetingRepository englishGreetingRepository) {
+        return new I18nEnglishGreetingService(englishGreetingRepository);
     }
 
     @Primary
     @Bean
-    PrimaryGreetingService primaryGreetingService(){
+    PrimaryGreetingService primaryGreetingService() {
         return new PrimaryGreetingService();
     }
 
--- src/main/java/guru/springframework/sfgdi/services/I18nEnglishGreetingService.java
+++ src/main/java/guru/springframework/sfgdi/services/I18nEnglishGreetingService.java
@@ -3,6 +3,8 @@ package guru.springframework.sfgdi.services;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
+import guru.springframework.sfgdi.repositories.EnglishGreetingRepository;
+
 /**
  * Created by jt on 12/27/19.
  */
@@ -10,6 +12,15 @@ import org.springframework.stereotype.Service;
 // @Profile("EN")
 // @Service("i18nService")
 public class I18nEnglishGreetingService implements GreetingService {
+    
+    private final EnglishGreetingRepository englishGreetingRepository;
+
+    
+    public I18nEnglishGreetingService(EnglishGreetingRepository englishGreetingRepository) {
+        this.englishGreetingRepository = englishGreetingRepository;
+    }
+
+
     @Override
     public String sayGreeting() {
         return "Hello World - EN";
